f5649b66099a30e778f44beee31750a3dd1e22b8
malikgarnette <38760178+malikgarnette@users.noreply.github.com>
2018-05-01 11:03:33 -0400

    Different bacground

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ function setup() {
 
 //The draw function happens over and over again
 function draw() {
-  background(0,0,76); //an RGB color for the canvas' background (dark blue)
+  background(2,6,100); //an RGB color for the canvas' background (dark blue)
   //moon
   fill(255,200); //white, 200 level of transparency (min = 0, max = 255)
   noStroke();
